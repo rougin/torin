4482a85fa8616ba9d0fc872cfe46203392c61d5c
Rougin Gutib <rougingutib@gmail.com>
2025-09-28 10:56:52 +0800

    Add unit tests for "ClientDepot"

--- tests/Depots/ClientDepotTest.php
+++ tests/Depots/ClientDepotTest.php
@@ -60,6 +60,59 @@ class ClientDepotTest extends Testcase
     /**
      * @return void
      */
+    public function test_can_create_client()
+    {
+        $data = ['name' => 'New Client', 'type' => 0, 'remarks' => 'Some remarks'];
+
+        $client = $this->depot->create($data);
+
+        $this->assertNotNull($client);
+        $this->assertEquals('New Client', $client->name);
+        $this->assertEquals(0, $client->type);
+        $this->assertEquals('Some remarks', $client->remarks);
+        $this->assertNotNull($client->code);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_can_get_clients_for_select()
+    {
+        $this->depot->create(['name' => 'Client X', 'type' => 0]);
+        $this->depot->create(['name' => 'Client Y', 'type' => 1]);
+
+        $items = $this->depot->getSelect();
+
+        $this->assertCount(2, $items);
+        $this->assertEquals(['value' => 1, 'label' => 'Client X'], $items[0]);
+        $this->assertEquals(['value' => 2, 'label' => 'Client Y'], $items[1]);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_can_update_client()
+    {
+        $client = $this->depot->create(['name' => 'Old Client Name', 'type' => 0, 'remarks' => 'Old remarks']);
+
+        $data = array('type' => 1);
+        $data['name'] = 'Updated Client Name';
+        $data['remarks'] = 'Updated remarks';
+
+        $result = $this->depot->update($client->id, $data);
+
+        $this->assertTrue($result);
+
+        $actual = $this->depot->find($client->id);
+
+        $this->assertEquals('Updated Client Name', $actual->name);
+        $this->assertEquals(1, $actual->type);
+        $this->assertEquals('Updated remarks', $actual->remarks);
+    }
+
+    /**
+     * @return void
+     */
     protected function doSetUp()
     {
         parent::doSetUp();
