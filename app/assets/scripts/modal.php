a25b569314f6e71ae070903763796841c916522a
Rougin Gutib <rougingutib@gmail.com>
2025-11-12 10:33:59 +0800

    Move "cuel-alert-modal" to "alert.php"

--- app/assets/scripts/modal.php
+++ app/assets/scripts/modal.php
@@ -1,16 +1,1 @@
-<div class="modal fade" id="cuel-alert-modal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div id="cuel-alert-modal-header">
-        <span class="modal-title fs-5 fw-bold" id="cuel-alert-modal-title"></span>
-      </div>
-      <div class="modal-body" id="cuel-alert-modal-body"></div>
-      <div class="modal-footer border-top-0 bg-light">
-        <div class="me-auto"></div>
-        <button type="button" class="btn btn-link text-secondary text-decoration-none" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <script type="text/javascript">const Modal={get(t){const e=document.getElementById(t);return bootstrap.Modal.getOrCreateInstance(e)},hide(t){return this.get(t).hide()},show(t){return this.get(t).show()},toggle(t){return this.get(t).toggle()}};</script>
